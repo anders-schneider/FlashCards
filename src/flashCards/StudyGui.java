f76294d21625d0f2a0121a9ee04b616e95644b22
anders-schneider <aschn@seas.upenn.edu>
2014-11-17 09:01:35 -0500

    Created the start window

--- src/flashCards/StudyGui.java
+++ src/flashCards/StudyGui.java
@@ -3,19 +3,37 @@
  */
 package flashCards;
 
-import javax.swing.JFrame;
+import javax.swing.*;
+
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
 /**
- * @author Your Name Goes Here
+ * @author Anders Schneider
  */
 public class StudyGui extends JFrame {
 
-    /**
-     * @param args
-     */
+	JButton loadButton;
+	
     public static void main(String[] args) {
-        // TODO Auto-generated method stub
-
+    	new StudyGui().createGUI();
     }
-
+    
+    void createGUI(){
+    	setSize(200, 100);
+    	setLayout(new BorderLayout());
+    	loadButton = new JButton ("Load study list");
+    	add(loadButton, BorderLayout.SOUTH);
+    	loadButton.addActionListener(new LoadButtonListener());
+    	add(new JLabel("Let's get started!"), BorderLayout.NORTH);
+    	setVisible(true);
+    }
+    
+    class LoadButtonListener implements ActionListener{
+		@Override
+		public void actionPerformed(ActionEvent event) {
+					
+		}
+    }
 }
