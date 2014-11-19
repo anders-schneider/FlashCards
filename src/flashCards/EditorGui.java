9cc46811a68b8b0133e7133ed9715d575c7e7808
anders-schneider <aschn@seas.upenn.edu>
2014-11-19 14:10:30 -0500

    StudyGui still all but functional
    Saves not working, and still not sure how to quit, but I'm about to
    switch everything over from creating new StudyGui to creating new
    JFrames

--- src/flashCards/EditorGui.java
+++ src/flashCards/EditorGui.java
@@ -3,19 +3,82 @@
  */
 package flashCards;
 
-import javax.swing.JFrame;
+import javax.swing.*;
+
+import simpleIO.SimpleIO;
+
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.awt.event.WindowEvent;
+import java.io.IOException;
+import java.util.ArrayList;
 
 /**
  * @author Your Name Goes Here
  */
 public class EditorGui extends JFrame {
 
-    /**
-     * @param args
-     */
+	static EditorGui editorGui;
+	
+	JLabel keySearch;
+	JTextField searchTextField;
+	JLabel searchResultsLabel;
+	JTextArea searchResultsTextArea;
+	JLabel studyListLabel;
+	JList<String> studyListDisplay;
+	JScrollPane studyListDisplayScrollable;
+	JButton deleteButton;
+	JButton addButton;
+	JButton editButton;
+	JButton saveButton;
+	JButton saveAsButton;
+	
     public static void main(String[] args) {
-        // TODO Auto-generated method stub
-
+    	editorGui = new EditorGui();
+    	editorGui.createWindow();
     }
+
+	private void createWindow() {
+		setSize(400, 500);
+		setVisible(true);
+		int rows = 7;
+		int columns = 2;
+		int separation = 30;
+		setLayout(new GridLayout(rows, columns, separation, separation));
+		
+		keySearch = new JLabel("Keyword Search:");
+		searchTextField = new JTextField();
+		searchResultsLabel = new JLabel("Results:");
+		searchResultsTextArea = new JTextArea();
+		studyListLabel = new JLabel("Study List:");
+		String[] jListString = {"A", "B", "C", "D"};
+		studyListDisplay = new JList<String>(jListString);
+		studyListDisplayScrollable = new JScrollPane(studyListDisplay);
+		deleteButton = new JButton("Delete");
+		addButton = new JButton("Add");
+		editButton = new JButton("Edit");
+		saveButton = new JButton("Save");
+		saveAsButton = new JButton("Save as");
+		
+		add(keySearch);
+		add(searchTextField);
+		add(searchResultsLabel);
+		add(searchResultsTextArea);
+		add(studyListLabel);
+		add(studyListDisplayScrollable);
+		add(new JPanel());
+		add(deleteButton);
+		add(new JPanel());
+		add(addButton);
+		add(new JPanel());
+		add(editButton);
+		add(saveButton);
+		add(saveAsButton);
+		
+		searchResultsTextArea.setEditable(false);
+		
+    	setDefaultCloseOperation(editorGui.EXIT_ON_CLOSE);
+	}
 
 }
